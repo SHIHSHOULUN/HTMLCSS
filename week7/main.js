22ad978e4f5caa7fc388ccdc34be5334335a1e83
SHIHSHOULUN <123545334+SHIHSHOULUN@users.noreply.github.com>
2024-09-08 20:03:07 +0800

    Add files via upload

---
+++ week7/main.js
@@ -0,0 +1,30 @@
+// AOS.init();  //AOS 動畫初始語法
+
+
+//依照客戶要求去做整體化更動設定 ex Duration:400 (0.4秒)改成300 (0.3秒)
+//若有個別區塊動畫要求,再從html逐一增加單一動畫語法
+
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: /*400 改秒數*/ 500, // values from 0 to 3000, with step 50ms     
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation 
+  
+  });  
+  
+  //Github Read Me:裡有詳細
